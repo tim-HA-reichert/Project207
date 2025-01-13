cd74173d36991e43ae468481d68f2b2a6edaaad8
Christian Simonsen <crsdev@gmail.com>
2025-01-13 11:49:05 +0100

    La til statuskoder for http

--- script.mjs
+++ script.mjs
@@ -1,15 +1,17 @@
 import express from 'express'
+import HTTP_CODES from './utils/httpCodes.mjs';
 
 const server = express();
 const port = (process.env.PORT || 8000);
 
 server.set('port', port);
 server.use(express.static('public'));
-server.get("/", (req, res, next) => {
 
-    res.status(200).send('Hello World').end();
+function getRoot(req, res, next) {
+    res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
+}
 
-});
+server.get("/", getRoot);
 
 server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
