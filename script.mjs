c728ebb949b4d657492f030ac8b020f75c070e92
Tim <tim.reichert111@gmail.com>
2025-01-16 11:29:39 +0100

    added basic additioning script. ~cRemoved unneeded folders.

--- script.mjs
+++ script.mjs
@@ -1,7 +1,8 @@
 import express from 'express'
 import HTTP_CODES from './utils/httpCodes.mjs';
-import randomQuote from './tmp/quotes/quotes.mjs';
-import printPoem from './tmp/poem/poem.mjs';
+import randomQuote from './tmp/quotes.mjs';
+import printPoem from './tmp/poem.mjs';
+import calculateNumbers from './tmp/sum.mjs';
 
 const server = express();
 const port = (process.env.PORT || 8080);
@@ -9,32 +10,53 @@ const port = (process.env.PORT || 8080);
 server.set('port', port);
 server.use(express.static('public'));
 
-let quote = randomQuote();
+let generateQuote = randomQuote();
 const poem = printPoem();
 
 setInterval(() => {
-    quote = randomQuote();  
+    generateQuote = randomQuote();  
 }, 2000); 
 
 function getQuote(req, res, next) {
-    res.status(HTTP_CODES.SUCCESS.OK).send(quote).end();
-}
-
-function getPoem(req, res, next) {
-    res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
+    res.status(HTTP_CODES.SUCCESS.OK).send(generateQuote).end();
 }
 
 function getRoot(req, res, next) {
-    res.status(HTTP_CODES.SUCCESS.OK).send("For quotes: /tmp/quotes <br> For poem: /tmp/poem").end();
+    res.status(HTTP_CODES.SUCCESS.OK).send(
+        `For quotes: /tmp/quotes <br> 
+        For poem: /tmp/poem <br>
+        For basic maths: /tmp/sum/a/b/ where "a" and "b" are numbers`
+    ).end();
 }
 
 server.get("/", getRoot);
+
+//Tester to foskjellige måter å skrive dette på 
 server.get("/tmp/quotes", getQuote);
-server.get("/tmp/poem", getPoem);
+server.get("/tmp/poem", (req, res, next) => {
+    res.status(HTTP_CODES.SUCCESS.OK).send(poem).end();
+});
+
+server.get("/tmp/sum/:a/:b/", (req, res)  => {
+
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+
+    const calculateParams = calculateNumbers(a, b);
+
+    if(!calculateParams){
+        return res.status(400).send("a and b must be numbers, man.")
+    }
+
+    res.status(HTTP_CODES.SUCCESS.OK).send(`sum: ${calculateParams} <br><br> 
+        type some new numbers in the searchbar for a new answer.
+        `).end();
+});
 
 
 server.listen(server.get('port'), function () {
     console.log(`Server running on http://localhost:${port}`);
     console.log(`Poem at http://localhost:${port}/tmp/poem`);
     console.log(`Quotes at http://localhost:${port}/tmp/quotes`);
+    console.log(`Calculate at http://localhost:${port}/tmp/sum/a/b/`);
 });
---
+++ tmp/sum.mjs
@@ -0,0 +1,7 @@
+const calculateNumbers = (a, b) => {
+    if(typeof a === "number" && typeof b === "number"){
+        return a + b
+    }
+};
+
+export default calculateNumbers;
