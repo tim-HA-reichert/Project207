533b1e9a68bb8369bc5f7038e917ad4e80e8bc87
Tim <tim.reichert111@gmail.com>
2025-01-22 09:43:18 +0100

    moved around some code. Reworked the cardDeck script. Added a index.html and style.css file

---
+++ cardDeck/deck.mjs
@@ -0,0 +1,29 @@
+//A deck of cards
+
+//4 difference arrays
+
+const cardDeck = {
+    "Hearts": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"],
+    "Spades": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"],
+    "Clubs": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"],
+    "Diamonds": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
+};
+
+const pickACard = (aDeck) => {
+    const suits = Object.keys(aDeck);
+    
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomCard = Math.floor(Math.random () * 13);
+    
+    const chosenCard = aDeck[randomSuit][randomCard];
+    
+    //At switch-case for the different numbers. Case 12 = queen or whatever.
+        console.log(`${chosenCard} of ${randomSuit}`);
+        return chosenCard
+    
+        //Add splice logic for chosen cards. 
+}
+
+pickACard(cardDeck);
+
+
