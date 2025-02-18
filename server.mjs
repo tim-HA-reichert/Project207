238f8daa96c3d9fa38a79fe2e24a331dd5bf8472
Tim <tim.reichert111@gmail.com>
2025-02-18 13:36:07 +0100

    formating

--- server.mjs
+++ server.mjs
@@ -3,7 +3,6 @@ import HTTP_CODES from './utils/httpCodes.mjs';
 import recipeRouter from './router/recipeRoutes.mjs';
 import loginRouter from './router/loginRoutes.mjs';
 
-
 const server = express();
 const port = (process.env.PORT || 8000);
 
@@ -17,8 +16,6 @@ server.use('/authentication', loginRouter);
 server.get("/", (req, res) => {
     res.status(HTTP_CODES.SUCCESS.OK).send().end();
 });
-
-
 
 
 server.listen(server.get('port'), function () {
