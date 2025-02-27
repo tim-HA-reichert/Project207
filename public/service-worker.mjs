fa3a1bc945b47bffbc45c8c38b51fe3ead676e74
Tim <tim.reichert111@gmail.com>
2025-02-27 11:59:47 +0100

    Added manifest info. Connected manifest to HTML. Updated CACHE_VERSION. Added icons.

--- public/service-worker.mjs
+++ public/service-worker.mjs
@@ -14,7 +14,7 @@ const HTTP_CODES = {
     }
 }
 
-const CACHE_VERSION = "v1";
+const CACHE_VERSION = "v2";
 
 const addResourceToCache = async (resources) => {
     const cache = await caches.open(CACHE_VERSION);
