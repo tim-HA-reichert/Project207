cb6f0320c9bbac0c6b18bba5cd2e5f7667307bbb
Tim <tim.reichert111@gmail.com>
2025-03-05 14:09:30 +0100

    added foundations of CRUD database functions

---
+++ data/recipeCalls.mjs
@@ -0,0 +1,18 @@
+
+
+class Recipe{
+    constructor(id, authorID, recipeTitle, nationality, cookingTime, difficulty){
+        this.id = id;
+        this.authorID = authorID;
+        this.recipeTitle = recipeTitle;
+        this.nationality = nationality;
+        this.cookingTime = cookingTime;
+        this.difficulty = difficulty; 
+    }
+
+    read(id){};
+    update(id, authorID){};
+    deletion(id, authorID){};
+
+
+}
