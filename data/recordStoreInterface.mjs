4a201bdc27b69c8e2f5f5b8c13ba71df9bcc60ee
Tim <tim.reichert111@gmail.com>
2025-03-06 11:26:42 +0100

    Added recordStore files

---
+++ data/recordStoreInterface.mjs
@@ -0,0 +1,30 @@
+function RecordStoreInterface(){
+    return{
+        create,
+        read,
+        update,
+        remove
+    }
+}
+
+
+function create(id){
+    throw new Error("WIP")
+}
+
+
+function update(item){
+    throw new Error("WIP")
+}
+
+
+function read(item){
+    throw new Error("WIP")
+}
+
+
+function remove(item){
+    throw new Error("WIP")
+}
+
+export default RecordStoreInterface
