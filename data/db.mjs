633e616aa003e1d114026824e06d2eb0a21456c3
Tim <tim.reichert111@gmail.com>
2025-03-08 11:47:35 +0100

    Added CRUD functions in for db. Imported CRUD functions into RecipeStore.mjs.

--- data/db.mjs
+++ data/db.mjs
@@ -1,12 +1,42 @@
 import pg from 'pg'
-const { Client } = pg
+const { Client, Pool } = pg
 
 const config = {
     connectionString: process.env.DB_EXTERNAL_CRED,
     ssl: any, 
 }
 
+async function create(statement, ...values){
+    return await runQuery(statement, ...values);
+}
+async function read(statement, ...values){
+    return await runQuery(statement, ...values);
+}
+async function update(statement, ...values){
+   return await runQuery(statement, ...values);
+}
+async function remove(statement, ...values){
+    return await runQuery(statement, ...values);
+}
 
-const client = new Client(config);
+async function runQuery(query, ...values){
+    const client = new Client(config);
+    
+    try{
+        await client.connect();
+        const result = await client.query(query, [...values]);
 
-await client.connect();
+        if(result.rowCount <= 0){
+            throw new Error(console.log("Row count is 0. No records created."))
+        }
+
+        return result.rows[0];
+
+    } catch(error){
+        console.error(error);
+        return null
+
+    }finally{
+       await client.end();
+    }
+}
--- data/recipeRecordStore.mjs
+++ data/recipeRecordStore.mjs
@@ -1,4 +1,5 @@
 import RecordStoreInterface from "./abstractRecordStoreInterface.mjs";
+import { create, read, update, remove } from './db.mjs';
 
 class RecipeStore extends RecordStoreInterface {
     
