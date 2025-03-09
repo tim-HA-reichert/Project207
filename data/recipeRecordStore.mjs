dd4640e4fc2615e27b697d4d1fbf0e01ad785b4d
Tim <tim.reichert111@gmail.com>
2025-03-09 15:14:24 +0100

    Added foundations of create method.

--- data/recipeRecordStore.mjs
+++ data/recipeRecordStore.mjs
@@ -1,6 +1,49 @@
 import RecordStoreInterface from "./abstractRecordStoreInterface.mjs";
-import { create, read, update, remove } from './db.mjs';
+import * as db from './db.mjs';
 
-class StoreRecipe extends RecordStoreInterface {
-    
+class StoreRecipeRecord extends RecordStoreInterface {
+
+    async create(recipe){
+        const query = `
+            INSERT INTO recipes (
+                title,
+                servings,
+                cookingTime,
+                difficulty,
+                mealType,
+                nationality,
+                ingredients,
+                instructions
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+            RETURNING *
+        `;
+
+        const values = [
+            recipe.title, 
+            recipe.servings, 
+            recipe.cookingTime, 
+            recipe.difficulty, 
+            recipe.mealType, 
+            recipe.nationality,
+            JSON.stringify(recipe.ingredients || []),
+            JSON.stringify(recipe.instructions || [])
+        ]
+
+        return await db.create(query, ...values);
+
+    }
+
+    read(){
+
+    }
+
+    update(){
+
+    }
+
+    remove(){
+
+    }
+
 }
