c5a4895c71c57d05e2fe9c04f8dcce0ce8dc6fa2
Tim <tim.reichert111@gmail.com>
2025-03-10 16:47:35 +0100

    made into class from function.

--- data/abstractRecordStoreInterface.mjs
+++ data/abstractRecordStoreInterface.mjs
@@ -1,30 +1,17 @@
-function RecordStoreInterface(){
-    return{
-        create,
-        read,
-        update,
-        remove
+export default class RecordStoreInterface {
+    async create(id) {
+        throw new Error("WIP - create method not implemented");
+    }
+
+    async read(item) {
+        throw new Error("WIP - read method not implemented");
+    }
+
+    async update(item) {
+        throw new Error("WIP - update method not implemented");
+    }
+
+    async remove(item) {
+        throw new Error("WIP - remove method not implemented");
     }
 }
-
-
-function create(id){
-    throw new Error("WIP")
-}
-
-
-function update(item){
-    throw new Error("WIP")
-}
-
-
-function read(item){
-    throw new Error("WIP")
-}
-
-
-function remove(item){
-    throw new Error("WIP")
-}
-
-export default RecordStoreInterface
