d10209c6fd0771f1fd3bcde6dee46354c11d928e
Tim <tim.reichert111@gmail.com>
2025-03-11 19:43:45 +0100

    Added missing export.

--- utils/recipe/index.mjs
+++ utils/recipe/index.mjs
@@ -1,4 +1,4 @@
 export { findRecipeById, searchRecipes } from './queries.mjs';
 export { validateRecipeData } from './validate.mjs';
-export { changeExistingRecipe } from './change.mjs';
+export { changeExistingRecipe, applyRecipeChanges } from './change.mjs';
 export { deleteRecipe } from './delete.mjs';
