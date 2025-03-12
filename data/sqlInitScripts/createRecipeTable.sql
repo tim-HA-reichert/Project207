a8a434760fa335804f54a50aa030cb46bb8e1bd7
Tim <tim.reichert111@gmail.com>
2025-03-12 21:10:54 +0100

    changed sql init

--- data/sqlInitScripts/createRecipeTable.sql
+++ data/sqlInitScripts/createRecipeTable.sql
@@ -2,14 +2,15 @@ CREATE TABLE recipes (
     recipe_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     servings INTEGER,
-    cookingTime INTEGER,
+    cooking_time INTEGER,
     difficulty VARCHAR(50),
-    mealType VARCHAR(50),
+    meal_type VARCHAR(50),
     nationality VARCHAR(50),
-    ingredients JSONB NOT NULL,
-    instructions JSONB NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    ingredients JSONB,
+    instructions JSONB
 );
+INSERT INTO recipes (title, servings, cooking_time, difficulty, meal_type, nationality, ingredients, instructions)
+VALUES ('Spaghetti Bolognese', 4, 30, 'Easy', 'Dinner', 'Italian', '[{"ingredient": "Spaghetti", "quantity": "200g"}, {"ingredient": "Ground Beef", "quantity": "500g"}]', '[{"step": "Boil water"}, {"step": "Cook spaghetti"}]');
 
 
 CREATE TABLE users (
