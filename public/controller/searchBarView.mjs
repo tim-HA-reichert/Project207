d737635ec6667ab8c91b7a0bf58ce0958d5ffeb9
Tim <tim.reichert111@gmail.com>
2025-03-13 16:33:10 +0100

    added event for when searchbar is empty. Added button for showing all recipes.

--- public/controller/searchBarView.mjs
+++ public/controller/searchBarView.mjs
@@ -1,4 +1,6 @@
 import TemplateManager from '../modules/templateManager.mjs';
+import renderAllRecipes from '../controller/showAllRecipeView.mjs'
+
 const templateFile = "./views/searchbarView.html";
 
 async function initSearchbar() {
@@ -11,20 +13,14 @@ async function initSearchbar() {
 
     const template = await TemplateManager.fetchTemplate(templateFile);
     const searchBarView = TemplateManager.staticCloneTemplate(template, header);
-
-    // Debug: Check what's in the template
-    console.log("Template content:", searchBarView);
     
-    // Get search elements - note template may have a root element
-    // Try different selector approaches
     const searchInput = searchBarView.querySelector("#recipe-search") || 
                         searchBarView.querySelector(".search-input");
     const searchButton = searchBarView.querySelector("#search-button") || 
                          searchBarView.querySelector(".search-button");
+    const showAllButton = searchBarView.querySelector("#show-all-recipes-button");
     
-    // Debug: Log what we found
-    console.log("Search input found:", !!searchInput);
-    console.log("Search button found:", !!searchButton);
+    console.log(showAllButton);
     
     if (searchInput && searchButton) {
         function performSearch() {
@@ -34,6 +30,11 @@ async function initSearchbar() {
                 const encodedTerm = encodeURIComponent(searchTerm);
                 
                 const newUrl = `${window.location.pathname}?search=${encodedTerm}`;
+                window.history.pushState({}, '', newUrl);
+                
+                window.dispatchEvent(new PopStateEvent('popstate'));
+            } else {
+                const newUrl = `${window.location.pathname}`;
                 window.history.pushState({}, '', newUrl);
                 
                 window.dispatchEvent(new PopStateEvent('popstate'));
@@ -51,15 +52,19 @@ async function initSearchbar() {
                 performSearch();
             }
         });
+
+        showAllButton.addEventListener("click", async (e) => {
+            e.preventDefault();
+            await renderAllRecipes();
+        })
+
     } else {
         console.error("No button or input found in the template:", templateFile);
         console.log("Template structure:", searchBarView.outerHTML);
     }
-
     return searchBarView;
 }
 
-// Create and export the controller with async initialization
 const searchViewController = await initSearchbar();
 
 export default searchViewController;
