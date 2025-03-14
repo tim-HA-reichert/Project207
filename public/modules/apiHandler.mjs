8c2feb9c60921a3d6f2a966095fc1e0b5cd156d8
Tim <tim.reichert111@gmail.com>
2025-03-14 04:36:48 +0100

    added delete call

--- public/modules/apiHandler.mjs
+++ public/modules/apiHandler.mjs
@@ -79,7 +79,15 @@ export async function updateRecipe(id, recipeChanges) {
 }
 
 export async function deleteRecipe(id) {
-    return runRequest(API_ENDPOINTS.RECIPES.Delete(id), HTTP_METHODS.DELETE);
+    const response = await runRequest(API_ENDPOINTS.RECIPES.Delete(id), HTTP_METHODS.DELETE);
+    console.log("API Response:", response); 
+
+    if(Array.isArray(response) && response.length > 0){
+        return response[0];
+    } else {
+        console.log(response)
+        return response;
+    }
 }
 
 export async function searchRecipes(searchCriteria) {
